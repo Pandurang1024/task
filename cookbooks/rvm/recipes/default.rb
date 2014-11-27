98829d8fc81b7d5fca22e7e1e2999885b1de152a
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-27 14:46:34 +0530

    Added

---
+++ cookbooks/rvm/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#rvm get stable --autolibs=enable
+#rvm install ruby
+#rvm --default use ruby-2.1.5
+
+execute 'rvmkey' do
+  cwd '/opt'
+  command 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'
+end
+
+
+execute 'rvmInstall' do
+cwd '/opt'
+command '\curl -sSL https://get.rvm.io | bash -s stable --ruby'
+end
+
