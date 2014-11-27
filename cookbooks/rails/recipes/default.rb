a3ae3ecb2a8a5cef660c09e95b1284eced8a38c0
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-27 19:30:57 +0530

    Added\

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -15,3 +15,83 @@ package "rails" do
   version "4.1.0"
   action :install
 end
+
+
+directory "/var/chef/prime_factors_kata/tmp" do
+ owner 'root'
+ group 'root'
+ mode '0755'
+ action :create
+end
+
+%w[ /var/chef/prime_factors_kata/db /var/chef/prime_factors_kata/db/migrate /var/chef/prime_factors_kata/tmp /var/chef/prime_factors_kata/log /var/chef/prime_factors_kata/Gemfile.lock ].each do |path|
+  directory path do
+    owner 'root'
+    group 'root'
+    mode '0777'
+  end
+end
+
+
+%w[ /var/chef/prime_factors_kata/db/seeds.rb /var/chef/prime_factors_kata/db/schema.rb ].each do |path|
+  directory path do
+    owner 'root'
+    group 'root'
+    mode '0777'
+  end
+end
+
+
+
+#Star pimefact rail server
+cookbook_file "/var/chef/prime_factors_kata/db/development.sqlite3" do
+  source "development.sqlite3"
+  mode "0677"
+end
+
+
+#Star pimefact rail server
+cookbook_file "/etc/init.d/primefact.sh" do
+  source "primefact.sh"
+  mode "0677"
+end
+
+cookbook_file "/opt/reqfiles.sh" do
+  source "reqfiles.sh"
+  mode "0677"
+end
+
+package "ruby-railties" do
+  action :install
+end
+
+package "libsqlite3-dev" do
+  action :install
+end
+
+execute 'updatesqlite' do
+  cwd '/var/chef/prime_factors_kata'
+  command 'bundle update sqlite3'
+end
+
+
+
+execute 'updatedb' do
+  cwd '/var/chef/prime_factors_kata'
+  command 'rake db:migrate'
+end
+
+cookbook_file "/opt/startapp.sh" do
+  source "startapp.sh"
+  mode "0677"
+end
+
+#bash "startapp" do
+ # guard_interpreter :bash
+ #code "opt/startapp.sh"
+ #end
+#Start Rails Server
+execute 'startRailServer' do
+  cwd '/var/chef/prime_factors_kata'
+  command 'rails s -d'
+end
