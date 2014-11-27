14eafaba426fb7785cebbad117d1b53b864272ca
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-27 21:32:30 +0530

    Added

--- cookbooks/myApp/files/default/gitcode.sh
+++ cookbooks/myApp/files/default/gitcode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #sudo git clone https://github.com/Pandurang1024/sys.git /var/chef/code
-cd /var/chef/code
+cd /var/chef1/code
 cp  -a  prime_factors_kata /var/chef/
-cd /var/chef/code/rvmparts
+cd /var/chef1/code/rvmparts
 cat rvm-part* > rvm.zip
 cp rvm.zip /usr/local
 cd /usr/local/
