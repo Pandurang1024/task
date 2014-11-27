471bf1cce18141428c2582558719979e31238c77
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-27 21:34:20 +0530

    Added

--- cookbooks/jenkins/files/default/installGems.sh
+++ cookbooks/jenkins/files/default/installGems.sh
@@ -1,13 +1,13 @@
 source /usr/local/rvm/scripts/rvm
 cd /var/lib/jenkins/jobs
-unzip /var/chef/code/primefactors.zip
+unzip /var/chef1/code/primefactors.zip
 chown -R jenkins:jenkins primefactors
 cd /var/lib/jenkins/plugins
-cp -r -f /var/chef/code/jenkins_plugins/* .
+cp -r -f /var/chef1/code/jenkins_plugins/* .
 chown -R jenkins:jenkins *
 cd /var/lib/jenkins
-cp /var/chef/code/credentials.xml .
-cp /var/chef/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml .
+cp /var/chef1/code/credentials.xml .
+cp /var/chef1/org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml .
 chown jenkins:jenkins credentials.xml
 chown jenkins:jenkins org.jenkinsci.plugins.xvfb.XvfbBuildWrapper.xml
 sudo service jenkins restart
