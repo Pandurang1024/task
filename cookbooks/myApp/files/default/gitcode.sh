17dddb47b86d05eaf5527ee501f9cdd26c6da81c
unknown <panduranga@ULTP_147.synerzipune.local>
2014-11-27 14:32:40 +0530

    Added

---
+++ cookbooks/myApp/files/default/gitcode.sh
@@ -0,0 +1,1 @@
+sudo git clone https://github.com/Pandurang1024/sysconfig.git /var/chef/code
--- cookbooks/myApp/recipes/default.rb
+++ cookbooks/myApp/recipes/default.rb
@@ -8,11 +8,15 @@
 #
 
 
-git "/var/chef/code" do                            
-    repository "git@github.com:Pandurang1024/sysconfig.git"
-    revision "master"                                 
-    #checkout_branch 'development'
-    action :sync                                     
-    user "root"                                                                  
+cookbook_file "/opt/gitcode.sh" do
+  source "gitcode.sh"
+  mode "0677"
 end
+
+bash "gitcode" do
+  guard_interpreter :bash
+  code "/opt/gitcode.sh"
+end
+
+
 
